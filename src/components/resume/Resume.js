46dd6c94a4eb16b04911d44397ad93a4b01a7f82
evs <erick.vs@icloud.com>
2017-08-29 16:45:44 -0700

    Resume page completed

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
+import './Resume.css'
 
 function Resume(props) {
 	return (
 		<div className="container">
+			<div className="resume-header">
+				<Button href="/resume.pdf" className='download-btn'>
+						<h2>Download copy</h2>
+						<FontAwesome 
+									className='download-color'
+									name='download' 
+									size='lg'
+									/>
+					</Button>
+			</div>
 			<div className="row">
 				<div className="col-md-12">
-					<div style={{height: '500px',backgroundImage: `url('http://erickvazquez.com/resume.svg')`}}>
-						
-					</div>
-					<iframe src="http://docs.google.com/gview?url=http://www.erickvazquez.com/resume.pdf&embedded=true" 
-					style={{width:'500px', height:'500px'}} title='ericksresume' frameborder="0"></iframe>
+					<div id='resume-container'></div>
 				</div>
 			</div>
 		</div>
