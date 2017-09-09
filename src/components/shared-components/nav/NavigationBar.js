8004f49d23da6b903cd20fe03a5ae0170ea95388
evs <erick.vs@icloud.com>
2017-09-09 01:47:40 -0700

    Fixed external link issue with NavItem

--- src/components/shared-components/nav/NavigationBar.js
+++ src/components/shared-components/nav/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
+import NavItem from './NavItem/NavItem' // Until official patch is released.
 import './NavigationBar.css'
 import logo from './logo.svg'
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap'
@@ -11,11 +12,7 @@ function NavigationBar(props) {
 		    <Navbar.Header>
 		      <Navbar.Brand>
 		        <a href="/" >
-							<img 
-								id='logo'
-								src={logo} 
-								alt='erick vazquez'
-								height='30px'/>
+							<img id='logo' src={logo} alt='erick vazquez' height='30px'/>
 		        </a>
 		      </Navbar.Brand>
 		      <Navbar.Toggle />
@@ -37,15 +34,15 @@ function NavigationBar(props) {
 		        <LinkContainer to='/contact'>
 		        	<NavItem>Contact</NavItem>
 		        </LinkContainer>
-		        <NavItem 	eventKey={1} 
-		        					href='https://www.linkedin.com/in/erickvazquez/' 
-		        					className='social-media-links' >
-		        	<FontAwesome name='linkedin-square' size='2x'></FontAwesome>
+		        <NavItem eventKey={1} 
+		        				 href='https://www.linkedin.com/in/erickvazquez/' 
+		        				 className='social-media-links' >
+		        	<FontAwesome name='linkedin-square' size='2x' />
 		        </NavItem>
-		        <NavItem 	eventKey={2} 
-		        					href="https://github.com/erickvs" 
-		        					className='social-media-links' >
-		        	<FontAwesome name='github' size='2x'></FontAwesome>
+		        <NavItem eventKey={2} 
+		        				 href="https://github.com/erickvs" 
+		        				 className='social-media-links' >
+		        	<FontAwesome name='github' size='2x' />
 		        </NavItem>
 		      </Nav>
 		    </Navbar.Collapse>
